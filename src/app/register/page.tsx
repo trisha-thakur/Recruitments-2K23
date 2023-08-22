8497f083a325972c018238ca651940a8d4decca3
Anubhav Pathak <anubhavpathak99@gmail.com>
2023-08-23 01:18:03 +0530

    Updated Register Page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import React, {useRef} from 'react'
+import React, {useRef, useState} from 'react'
 import Input from '@/components/ui/Input'
+import Select from '@/components/ui/Select';
 import { validateInput } from '@/utils/value';
-
 
 const Page = () => {
 
@@ -15,6 +15,8 @@ const Page = () => {
   const passwordRef = useRef<HTMLInputElement>(null)
   const confirmPasswordRef = useRef<HTMLInputElement>(null)
 
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+
   const submitHandler = (e: React.FormEvent) => {
     e.preventDefault()
     const name = nameRef.current?.value.trim()
@@ -24,24 +26,46 @@ const Page = () => {
     const phone = phoneRef.current?.value.trim()
     const password = passwordRef.current?.value.trim()
     const confirmPassword = confirmPasswordRef.current?.value.trim()
+    const domain = selectedOption
 
-    if (!validateInput(name, 'name')) {
-      nameRef.current?.focus()
-      return
-    }
+    // Perform validation
+
+
+    // Send data to server
+
+    // Clear form
+    nameRef.current!.value = ''
+    registerNoRef.current!.value = ''
+    emailRef.current!.value = ''
+    srmEmailRef.current!.value = ''
+    phoneRef.current!.value = ''
+    passwordRef.current!.value = ''
+    confirmPasswordRef.current!.value = ''
+    setSelectedOption(null)
+
+    // Show success message
+    
+    // Redirect to login page
+    window.location.href = '/'
+
   }
 
   return (
-    <main className='p-4'>
-      <h1 className='font-f1 font-bold text-5xl'>Register</h1>
+    <main className='px-[4vw] py-4'>
+      <h1 className='font-f1 font-bold text-5xl mb-8'>Register</h1>
       <form onSubmit={submitHandler}>
-        <Input ref={nameRef} label='Name' placeholder='Type Here...' type='text'/>
-        <Input ref={registerNoRef} label='Register Number' placeholder='Type Here...' type='text'/>
-        <Input ref={emailRef} label='Personal Email' placeholder='Type Here...' type='email'/>
-        <Input ref={srmEmailRef} label='SRM Email' placeholder='Type Here...' type='email'/>
-        <Input ref={phoneRef} label='Phone Number' placeholder='Type Here...' type='text'/>
-        <Input ref={passwordRef} label='Password' placeholder='Type Here...' type='password'/>
-        <Input ref={confirmPasswordRef} label='Confirm Password' placeholder='Type Here...' type='password'/>
+        <Input ref={nameRef} label='Name' placeholder='John Doe' type='text'/>
+        <Input ref={registerNoRef} label='Register Number' placeholder='RA2XXXXXXXXXXXX' type='text'/>
+        <Input ref={emailRef} label='Personal Email' placeholder='xyz@gmail.com' type='email'/>
+        <Input ref={srmEmailRef} label='SRM Email' placeholder='xy000@srmist.edu.in' type='email'/>
+        <Input ref={phoneRef} label='Phone Number' placeholder='XXX-XXX-XXXX' type='text'/>
+        <Input ref={passwordRef} label='Password' placeholder='' type='password'/>
+        <Input ref={confirmPasswordRef} label='Confirm Password' placeholder='' type='password'/>
+        <div className='mt-4 max-w-screen-md'>
+          <label>Choose Domain</label>
+          <Select style="md:grid-cols-2 gap-2 mt-2" options={['Technical', 'Corporate', 'Creatives', 'Research']} setSelectedOption={setSelectedOption} selectedOption={selectedOption} />
+          <p className="mt-4"> Selected Option: {selectedOption ? selectedOption : 'None selected'}</p>
+        </div>
         <button type='submit' className="btn btn-primary mt-4 font-normal">Register</button>
       </form>
     </main>
--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -1,48 +1,24 @@
 import React, { useState } from 'react';
 
-const OptionBox: React.FC<{ label: string; onClick: () => void; isSelected: boolean }> = ({
-  label,
-  onClick,
-  isSelected,
-}) => {
+const OptionBox: React.FC<{ label: string; onClick: () => void; isSelected: boolean }> = ({label, onClick, isSelected,}) => {
   return (
-    <button
-      className={`p-4 border rounded-lg ${
-        isSelected
-          ? 'bg-blue-500 text-white border-blue-500'
-          : 'bg-gray-200 text-gray-700 border-gray-300 hover:bg-blue-100 hover:text-blue-500'
-      }`}
-      onClick={onClick}
-    >
-      {label}
-    </button>
+    <button className={`p-4 max-w-sm border rounded-lg hover:border-secondary ${isSelected ? 'bg-secondary' : ''}`} onClick={onClick}>{label}</button>
   );
 };
 
-const Select: React.FC = () => {
-  const [selectedOption, setSelectedOption] = useState<string | null>(null);
-
+const Select: React.FC<{
+  options: Array<string>, 
+  style: string, 
+  setSelectedOption: React.Dispatch<React.SetStateAction<string | null>>, 
+  selectedOption: string | null} > = ({options, style, setSelectedOption, selectedOption}) => {
   const handleOptionSelect = (option: string) => {
     setSelectedOption(option);
   };
-
-  const options = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
-
   return (
-    <div className="p-4">
-      <div className="grid grid-cols-2 gap-4">
-        {options.map((option, index) => (
-          <OptionBox
-            key={index}
-            label={option}
-            onClick={() => handleOptionSelect(option)}
-            isSelected={selectedOption === option}
-          />
-        ))}
+    <div>
+      <div className={`grid ${style}`}>
+        {options.map((option, index) => <OptionBox key={index} label={option} onClick={() => handleOptionSelect(option)} isSelected={selectedOption === option}/>)}
       </div>
-      <p className="mt-4">
-        Selected Option: {selectedOption ? selectedOption : 'None selected'}
-      </p>
     </div>
   );
 };
