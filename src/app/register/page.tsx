b2e3e137f9bbd77481a289da195d335348451e97
Anubhav Pathak <anubhavpathak99@gmail.com>
2023-08-28 21:38:52 +0530

    Removed bugs

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -14,8 +14,6 @@ const Page = () => {
   const { data: session } = useSession()
   const toast = useToast();
 
-  if(!session) window.location.href = '/'
-
   const names = session?.user?.name?.split(' ');
   const name = names ? names[0] + ' ' + names[1] : '';
   const registerNo = session?.user?.name?.split('(')[1]?.split(')')[0] || '';
